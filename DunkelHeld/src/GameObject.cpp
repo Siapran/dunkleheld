08592f355937a4a75b46a48abe8422dec941e5fb
siapran <siapran.candoris@gmail.com>
2015-02-02 16:34:59 +0100

    quickfix

---
+++ DunkelHeld/src/GameObject.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   GameObject.cpp
+ * Author: siapran
+ * 
+ * Created on February 2, 2015, 3:59 PM
+ */
+
+#include "GameObject.h"
+
+GameObject::GameObject() {
+}
+
+GameObject::GameObject(const GameObject& orig) {
+}
+
+GameObject::~GameObject() {
+}
+
---
+++ DunkelHeld/src/GameObject.h
@@ -0,0 +1,28 @@
+/* 
+ * File:   GameObject.h
+ * Author: siapran
+ *
+ * Created on February 2, 2015, 3:59 PM
+ */
+
+#ifndef GAMEOBJECT_H
+#define	GAMEOBJECT_H
+
+class GameObject {
+public:
+    GameObject();
+    GameObject(const GameObject& orig);
+    virtual ~GameObject();
+
+    
+    void update(std::vector<std::string>& events);
+    
+private:
+
+    std::string m_name;
+    std::vector<GameObject> m_children;
+    
+};
+
+#endif	/* GAMEOBJECT_H */
+
