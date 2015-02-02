fc3afd78d01a12715d34ff117fa591dd829c25df
siapran <siapran.candoris@gmail.com>
2015-02-02 14:54:17 +0100

    début Props, création convention de codage de maps

--- DunkelHeld/Prop.h
+++ DunkelHeld/Prop.h
@@ -8,41 +8,44 @@
 #ifndef PROP_H
 #define	PROP_H
 
+#include <SFML/Graphics.hpp>
 #include <map>
+#include <vector>
 
 class Prop : public sf::Drawable, public sf::Transformable {
 public:
+
     Prop();
     Prop(const Prop& orig);
     virtual ~Prop();
+
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+
 private:
-    
+
     std::string m_name;
     sf::Vector2f m_center;
 
-    std::vector<Frame> m_frames;
+    sf::Time m_frameTime; // temps entre chaque frame
+    std::vector<sf::Sprite> m_frames; // liste de frames
     sf::Texture *m_texture;
-    
-    std::map<std::string, PropState> m_states;
-};
 
-class PropState {
-public:
-    std::string name;
-
-private:
-    
-    std::map<std::string, std::string> m_descriptions;
-    std::vector<sf::FloatRect> m_hitBoxes;
-    std::map<std::string, PropAction> m_actions;
+    std::map<std::string, PropState> m_states; // nom état -> état
 
 };
 
-class PropAction {
-public:
+struct PropState {
     std::string name;
-public:
-    
+    std::map<std::string, std::string> descriptions; // langue -> description
+    std::vector<sf::FloatRect> hitBoxes; // liste de hitboxes
+    std::map<std::string, PropAction> actions; // nom action -> action
+    std::vector<int> animation; // liste d'id de frames
+};
+
+struct PropAction {
+    std::string name;
+    std::map<std::string, std::string> descriptions; // langue -> description
+    std::vector<int> animation; // liste d'id de frames
 };
 
 #endif	/* PROP_H */
