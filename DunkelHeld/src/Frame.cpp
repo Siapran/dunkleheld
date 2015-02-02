ae1eb4b1241f46dd672bd0ebd444ce7367095243
siapran <siapran.candoris@gmail.com>
2015-02-02 10:40:29 +0100

    modification structure tileset

---
+++ DunkelHeld/src/Frame.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   Frame.cpp
+ * Author: siapran
+ * 
+ * Created on February 2, 2015, 10:23 AM
+ */
+
+#include "Frame.h"
+
+Frame::Frame() {
+}
+
+Frame::Frame(const Frame& orig) {
+}
+
+Frame::~Frame() {
+}
+
---
+++ DunkelHeld/src/Frame.h
@@ -0,0 +1,28 @@
+/* 
+ * File:   Frame.h
+ * Author: siapran
+ *
+ * Created on February 2, 2015, 10:23 AM
+ */
+
+#ifndef FRAME_H
+#define	FRAME_H
+
+class Frame : public sf::Drawable, public sf::Transformable {
+public:
+    Frame();
+    Frame(const Frame& orig);
+    virtual ~Frame();
+
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+
+private:
+
+    int m_id;
+    sf::Vertex m_vertices[4];
+    const sf::Texture* m_texture;
+
+};
+
+#endif	/* FRAME_H */
+
